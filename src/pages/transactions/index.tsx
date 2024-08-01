5bf82b0935490022c0acf122ea1f3dc2161250a9
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-08-01 18:37:58 -0300

    feat: create transactions table

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '@/components/header'
 import { Summary } from '@/components/summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>01/08/2024</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>28/07/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
