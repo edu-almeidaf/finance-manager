d5e8021f998270e6ae1b65d1c94aad2dba6112de
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-08-01 19:54:08 -0300

    feat: create new transaction modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,6 @@
+import { NewTransactionModal } from '../new-transaction-modal'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header() {
   return (
@@ -6,7 +8,13 @@ export function Header() {
       <HeaderContent>
         <h1>Gerenciador de finanças</h1>
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
